79677c92956e72e22b616597a175b80c77ec7168
stensoomre <98737965+stensoomre@users.noreply.github.com>
2022-01-31 11:17:04 +0200

    Ülesanne 1
    Ülimalt raske võttis neli ööd ja neli hommikut aega, Kulutasin tunde oma elust selle ära tegemisest ja siis tuli meelde, et ma pole elektri maksu ja elekter kadus koos oma tööga ja pidin uuesti alustama, seda fantastilist tööd uuesti algusest ja pärast seda olin väga kurb ja nutsin, kuna unustasin uuesti. Siis läksin poodi ja lasin kodutul sellel ära teha.

---
+++ Ülesanne 1.py
@@ -0,0 +1,16 @@
+#Sten soomre
+#31.01.2022
+#Ülesanne 1
+
+print("\"Cabernet Sauvignon\" või \'Cabernet Sauvignon\'?")
+print(""" 
+  @..@
+ (----)
+( \__/ )
+^^ "" ^^
+""")
+
+ruut = int(input("Sisesta ruudu külje pikkus: "))
+pindala = ruut * ruut
+
+print("Vastus: ruudu pindala on " + str(pindala) + " cm^2")
