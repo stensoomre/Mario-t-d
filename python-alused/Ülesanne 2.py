1423c80c1a432978e3ea1b83f1c4d0a35fd8f295
stensoomre <98737965+stensoomre@users.noreply.github.com>
2022-02-01 13:47:30 +0200

    Add files via upload

--- python-alused/Ülesanne 2.py
+++ python-alused/Ülesanne 2.py
@@ -5,11 +5,17 @@
 from math import *
 
 # Kütuse maania
+# Sten Soomre
+# 02.02.22
 kyts = int(input("Siseta kulutatud kütus (Liitrides): "))
 maa = int(input("Siseta selle kütusega sõidetud maa (km): "))
 kulund1 = (maa/kyts)/100
 print("Kulutate 100 km kohta",kulund1,"L kütust")
+
 # Arvusüsteemid
+# Sten Soomre
+# 02.02.22
+
 numba = int(input("Siseta number : "))
 numba2 = bin(numba)
 print(numba2)
@@ -18,12 +24,18 @@ print(numba3)
     
 
 # Ajateisendus
+# Sten Soomre
+# 02.02.22
+
 minutid = int(input("Siseta minutite arv: "))
 aeg = minutid//60
 aeg2 = minutid-(aeg*60)
 print("Aeg oleks siis:",aeg,":",aeg2)
 
 # Hüpotenuus
+# Sten Soomre
+# 02.02.22
+
 a = 16
 b = 9
 c2 = a*a+b*b
@@ -31,12 +43,18 @@ c = round(sqrt(c2),2)
 print("Hüpetnuus on",c)
 
 # Rulluisutajad
+# Sten Soomre
+# 02.02.22
+
 kiirus = 29.9 #km/h
 aeg = 24 # minut
 kuluaeg = round(((kiirus/60)*aeg),2)
 print("Rulluikudega kiirusega" ,str(kiirus), "km/h jõuaks" ,aeg, "minutiga," ,kuluaeg, "km kaugusele")
 
 # Pitsa
+# Sten Soomre
+# 02.02.22
+
 soberad = 3
 Hind = 12.90
 Joot = 0.1
@@ -44,6 +62,9 @@ summa1 = round(soberad/(Hind*Joot),2)
 print("Iga üks peab jätma ",summa1,"€")
 
 # Toote hind
+# Sten Soomre
+# 02.02.22
+
 Toode = 36.75
 Soodus = 0.4
 Kogus = 3
@@ -51,6 +72,9 @@ summa = round(Kogus*(Toode-(Toode*Soodus)), 2)
 print(Kogus, "Toote hind on",summa, "€")
 
 # Kolmnurga ümbermõõt
+# Sten Soomre
+# 02.02.22
+
 a,b,c = 15,16,18
 
 Kolmnurk = 15+16+18
