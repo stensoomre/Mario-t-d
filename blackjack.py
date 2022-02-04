22a8d4c6f424d8e4ab41c9b11b82982dc86b7254
stensoomre <98737965+stensoomre@users.noreply.github.com>
2022-02-04 09:56:27 +0200

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -2,21 +2,8 @@
 
 from random import *
 
-
 from os import system, name
 from time import sleep
-def clear():
-  
-    # for windows
-    if name == 'nt':
-        _ = system('cls')
-  
-    # for mac and linux(here, os.name is 'posix')
-    else:
-        _ = system('clear')
-
-
-        
 
 def main():
     clear()
