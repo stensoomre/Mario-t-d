2076ad854b4f8ac4b43176700c1b97674e0ac4cb
stensoomre <98737965+stensoomre@users.noreply.github.com>
2022-02-18 16:38:53 +0200

    Update Ülesanne 10.py

--- python-alused/Ülesanne 10.py
+++ python-alused/Ülesanne 10.py
@@ -1,22 +1,39 @@
+# Harjutus 10
+# Sten Soomre
+# 18.02.2022
 import re
+import socket
 iplist = []
 plist = []
 valjad = open('valjad.txt')
 for line in valjad:
     ipaddress = "None"
     parool = "None"
-    if line.find(".") > 0:
+    if line.find(".") > 2: # Kui listis on . siis on IP
         ipaddress = line[:len(line) - 1]
-        iplist.append(ipaddress)
+        try: # Siis kui on ip
+            socket.inet_aton(ipaddress) 
+            iplist.append(ipaddress)
+        except socket.error: # Siis kui see ei ole ip
+            continue
     else:
-        parool = line[:len(line) - 1]
-        plist.append(parool)
-        for a in range(0,10):
-            if parool.find(str(a)) > 0:
-                print(parool)
-
-# print(iplist)
-# print(plist)
-
+        if line.find(".") > 0:
+            continue
+        else:
+            parool = line[:len(line) - 1]
+            if re.search('[a-z]', line): # otsib kas on tähti, väikseid
+                if re.search('[A-Z]', line): # otsib kas on tähti suuri
+                    if re.search('[0-9]', line): #otsib kas on numbreid
+                       plist.append(parool)
+            else: #juhul kui pole väikseid äkki on suuri
+                if re.search('[A-Z]', line): # otsib kas on tähti suuri
+                    if re.search('[0-9]', line): #otsib kas on numbreid
+                       plist.append(parool)
             
-     
+             
+print("IP-adressid")
+print(iplist)
+print()
+print("Paroolid")
+print(plist)
+
