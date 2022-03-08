4fc148b484b52cf01e4beb9195a89d496f239a4e
stensoomre <98737965+stensoomre@users.noreply.github.com>
2022-03-08 09:59:16 +0200

    Add files via upload

---
+++ too/Töö 4.py
@@ -0,0 +1,64 @@
+# Sten Soomre
+# Töö 4
+# 08.03.2022
+
+# Mündid
+
+mundid = open("mündid.txt", encoding="UTF-8")
+mundid2 = 0
+for line in mundid:
+    line = int(line)
+    if line <= 5:
+        mundid2 += line
+print(mundid2)
+
+
+# Tervitus
+
+def tervitus():
+    arv = int(input("Mitu külalist külastab? "))
+    number = 0
+    for i in range(arv):
+        number += 1 
+        print("Võõrustaja: \"Tere\"")
+        print(f"Täna {number}. kord tervitada, mõtiskleb võõrustaja.")
+        print("Külaline: \"Tere, suur tänu kutse eest!\"")
+    
+tervitus()
+# Peo eelarve
+
+def eelarve2():
+    inim = int(input("Mitu külalist tuleb? "))
+    inim2 = int(input("Mitu nendest on teatanud, et tuleb? "))
+    inim1 = inim - inim2
+    eelarve(inim, inim2)
+    
+def eelarve(inim, inim2):
+    raha = (10*inim+55)
+    raha2 = (10*inim2+55)
+    print("Minimaalne eelarve:",raha2,"eurot")
+    print("Maksimaalne eelarve:",raha,"eurot")
+    
+    
+eelarve2()
+
+# Õunamahla tegemine
+ounkogu = int(input("Sisestage mitu kilogrammi õunu on: "))
+mahlapakkarv = round((ounkogu*0.4/3),0)
+print(mahlapakkarv)
+
+# Banner
+
+def banner(vaide):
+    vaide = vaide.upper()
+    print(vaide)
+
+def banner2():
+    vaar1 = int(input("Mitu korda soovite reklaamlauset kuvada? "))
+    vaide = input("Sisestage reklaamlause: ")
+    
+    for i in range(vaar1):
+        banner(vaide)
+        
+banner2()
+ 
