7f5da4779afe53264c7feabfde08255c4a8453ca
stensoomre <98737965+stensoomre@users.noreply.github.com>
2022-04-18 11:08:10 +0300

    Add files via upload

--- Go-Dot Harjutused/Harj 8/Harj 8.gd
+++ Go-Dot Harjutused/Harj 8/Harj 8.gd
@@ -1,10 +1,64 @@
 extends Node
- 
-var summa = 0
- 
-func _process(delta):
-	$"Bg/Kast/Tekst".text = str(summa)
-	$"Bg/nupp".text = "Vajuta mind"
+var option
+var punkt = 0
+var valik
+func random(a,b): # funktioon oleks lihtsam
+	var rand = RandomNumberGenerator.new()#lühend
+	rand.randomize() # randomizib
+	var rand2 = rand.randi_range(a,b) #genib
+	return rand2 # returnib
 
-func _on_nupp_pressed():
-	summa += 1
+func _process(delta):
+	$"Bg/kivi".text = "Kivi"
+	$"Bg/paber".text = "Paber"
+	$"Bg/kaarid".text = "Käärid"
+	$"Kast/Punkte".text = "Punkte: "+str(punkt)	
+	
+func _on_kivi_pressed():
+	print("Kivi")
+	option = "Kivi"
+	pt2(option)
+
+func _on_paber_pressed():
+	print("Paber")
+	option = "Kivi"
+	pt2(option)
+
+func _on_kaarid_pressed():
+	print("Käärid")
+	option = "Kivi"
+	pt2(option)
+	
+func pt2(option):  # 3 = Kivi # 2 = Käärid # 1 = Paber
+	valik = random(1,3)
+	print(valik)
+	if option == "Kivi":
+		if valik == 2:
+			punkt += 1
+			$"Vastane/Tegi".text = "Vastane valis: Käärid"
+		elif valik == 3:
+			$"Vastane/Tegi".text = "Vastane valis: Kivi"
+		else:
+			punkt -= 1
+			$"Vastane/Tegi".text = "Vastane valis: Paber"
+	elif option == "Paber":
+		if valik == 3:
+			punkt += 1
+			$"Vastane/Tegi".text = "Vastane valis: Kivi"
+		elif valik == 1:
+			$"Vastane/Tegi".text = "Vastane valis: Paber"
+		else:
+			punkt -= 1
+			$"Vastane/Tegi".text = "Vastane valis: Käärid"
+	elif option == "Käärid":
+		if valik == 1:
+			punkt += 1
+			$"Vastane/Tegi".text = "Vastane valis: Paber"
+		elif valik == 2:
+			$"Vastane/Tegi".text = "Vastane valis: Käärid"
+		else:
+			punkt -= 1
+			$"Vastane/Tegi".text = "Vastane valis: Kivi"
+			
+		
+		
