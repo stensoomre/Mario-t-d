7f896995fe2c665907a921399ad4755712a7f8bd
stensoomre <98737965+stensoomre@users.noreply.github.com>
2022-10-08 00:58:29 +0300

    Update KT17.java

--- Java/KT/KT17.java
+++ Java/KT/KT17.java
@@ -1,19 +1,18 @@
-import java.util.Scanner;
-
 // Sten Soomre //
 class KT17 {
     public static void main(String[] args) {
-        int number, i;
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.print("Sisesta number: ");
-            number = sc.nextInt();
-        }
-        i = 2;
-        System.out.print("Paarisrvud: ");
-
-        while (i <= number) {
-            System.out.print(i + " ");
-            i = i + 2;
+        
+        int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        for(int number : numbers){
+            int i = 2;
+            if (number % 2 == 0){
+                System.out.print("Paarisrvud: ");
+                while (i <= number) {
+                    System.out.print(i + " ");
+                    i = i + 2;
+                }
+                System.out.println("");
+            }
         }
     }
 }
