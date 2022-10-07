01df211926dfcc1919f169fa133219e627420a40
stensoomre <98737965+stensoomre@users.noreply.github.com>
2022-10-08 01:30:36 +0300

    Update KT5.java

--- Java/KT/KT5.java
+++ Java/KT/KT5.java
@@ -4,48 +4,34 @@ import java.util.Scanner;
 class KT5 {
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Valige 1, et leida rööpselt ühendatud takiste kogutakistus - ");
-        String valik = scanner.next();
-        String arv1;
-        String arv2;
-        String arv3;
-        String arv4;
-        String arv5;
-        String arv6;
-
-        double s;
-
-        switch (Integer.parseInt(valik)) {
-            case 1:
-                System.out.print("Takistuse oom: ");
-                arv1 = scanner.next();
-                System.out.print("Takistuse oom: ");
-                arv2 = scanner.next();
-                System.out.print("Takistuse oom: ");
-                arv3 = scanner.next();
-                System.out.print("Takistuse oom: ");
-                arv4 = scanner.next();
-                System.out.print("Takistuse oom: ");
-                arv5 = scanner.next();
-                System.out.print("Takistuse oom: ");
-                arv6 = scanner.next();
-                s = jes(arv1, arv2, arv3, arv4, arv5, arv6);
-                System.out.print("Kogutakistus on " + s + " oomi");
-                break;
+        int i = 1;
+        System.out.println("A  -  Jadamisi\nB  -  Rööbiti");
+        String valik = scanner.nextLine();
+        if("B".equals(valik)){
+            System.out.print("Mitu takistit? ");
+            Integer number = scanner.nextInt();
+            int R1 = 0;
+            for (;i <= number;){
+                System.out.print("Mitu oomi? ");
+                Integer Rn = scanner.nextInt();
+                R1 = R1 + Rn;
+                i++;
+                }
+            float R = 10000f/R1;
+            System.out.println(R+" oomi");
         }
-
-    }
-
-    public static double jes(String a, String b, String c, String d, String e, String f) {
-        float arv1 = Float.parseFloat(a);
-        float arv2 = Float.parseFloat(b);
-        float arv3 = Float.parseFloat(c);
-        float arv4 = Float.parseFloat(d);
-        float arv5 = Float.parseFloat(e);
-        float arv6 = Float.parseFloat(f);
-        double s = (arv1 + arv2 + arv3 + arv4) + (arv5 * arv6 / (arv5 + arv6));
-        return s;
+        if("A".equals(valik)){ 
+            System.out.print("Mitu takistit? ");
+            Integer number = scanner.nextInt();
+            int R = 0;
+            for (;i <= number;){
+                System.out.print("Mitu oomi? ");
+                Integer Rn = scanner.nextInt();
+                R = R + Rn;
+                i++;
+            }
+            System.out.println(R+" oomi");
+        }
     }
 }
