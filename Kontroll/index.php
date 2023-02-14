790b7a31da46632cf47ddd4a053954c69f45019e
stensoomre <98737965+stensoomre@users.noreply.github.com>
2023-02-14 15:33:01 +0200

    Add files via upload

---
+++ Kontroll/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+    integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+    crossorigin="anonymous"></script>
+  <title>Kontroll Töö (aga tegelikkult Mariole lihtsalt meeldib inimesi kiusata)</title>
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light">
+
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="img/logo.png" style="width:30px;" alt="logo">
+        Firma
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
+        aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="me-auto"></ul>
+        <ul class="navbar-nav mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Avaleht</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Team</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+        </ul>
+        <button class="btn btn-outline-dark" type="submit">Button</button>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container"> <!-- Tee kunagi carousel-ga -->
+    <p class="fs-1" style="margin-top: 50px;font-weight: 600; margin-bottom: 50px;">
+      Great business are built on <span style="color: red;"> great experiences</span>. We make those experiences happen.
+    </p>
+    <button class="btn btn-outline-dark fs-3 me-2">
+      <i class="fa-solid fa-angle-left" style="width:20px; height: 30px;"></i>
+    </button>
+    <button class="btn btn-outline-dark fs-3">
+      <i class="fa-solid fa-angle-right" style="width:20px; height: 30px;"></i>
+    </button>
+  </div>
+  <div class="container mt-4">
+    <img src="img/Jumbo1.webp" class="img-fluid" alt="äge pilt">
+  </div> <!-- Tee kunagi carousel-ga -->
+
+  <div class="container mt-4">
+    <div class="display-1 fs-6 text-center">
+      Call to action text This is some text and button within a call-to-action text block.
+      <button class="ms-4 mr-1 btn btn-danger" style="border-radius: 4px; font-size: 14px; padding: 4px;">Call to Action</button>
+    </div>
+  </div>
+  <p class="text-center" style="margin-top: 40px;margin-bottom: -5px; font-weight: 700; font-size: 50px">Get<span
+      style="color: red;"> Creative</span></p>
+  <div class="container">
+    <div class="display-1 fs-5 text-center">
+      Vivamus sagittis lacus vel augue laoreet rutrum faucivus dolor auctor. <a style="text-decoration:none;" href="#">
+        Duis mollis</a>.
+    </div>
+  </div>
+  <div class="container text-center" style="margin-top: 60px;">
+    <div class="row">
+      <div class="col">
+        <img src="img/Grid1.jpg" class="img-fluid" alt="äge pilt">
+      </div>
+      <div class="col">
+        <img src="img/Grid2.jpg" class="img-fluid" alt="äge pilt">
+      </div>
+    </div>
+    <div class="row mt-4 mb-4">
+      <div class="col">
+        <img src="img/Grid3.jpg" class="img-fluid" style="height: 200px;" alt="äge pilt">
+      </div>
+      <div class="col">
+        <img src="img/Grid4.jpg" class="img-fluid" style="height: 200px;" alt="äge pilt">
+      </div>
+      <div class="col">
+        <img src="img/Grid5.jpg" class="img-fluid" style="height: 200px;" alt="äge pilt">
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
