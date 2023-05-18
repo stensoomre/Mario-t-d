bc8a7dd192f268bec78ae766d079c7f05bac7a88
stensoomre <98737965+stensoomre@users.noreply.github.com>
2023-05-18 11:39:04 +0300

    Add files via upload

---
+++ php.sten/kontrolltoo/index.php
@@ -0,0 +1,345 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+    integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+    crossorigin="anonymous"></script>
+  <title>Kontroll Töö (aga tegelikkult Mariole lihtsalt meeldib inimesi kiusata)</title>
+  <style>
+    a {
+      text-decoration-line: none;
+    }
+
+    .center {
+      text-align: center;
+    }
+  </style>
+</head>
+
+
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light">
+
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="img/logo.png" style="width:30px;" alt="logo">
+        Firma
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
+        aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="me-auto"></ul>
+        <ul class="navbar-nav mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Avaleht</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Team</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+        </ul>
+        <button class="btn btn-outline-dark" type="submit">Button</button>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container"> <!-- Tee kunagi carousel-ga -->
+    <p class="fs-1" style="margin-top: 50px;font-weight: 600; margin-bottom: 50px;">
+      Great business are built on <span style="color: red;"> great experiences</span>. We make those experiences happen.
+    </p>
+    <button class="btn btn-outline-dark fs-3 me-2">
+      <i class="fa-solid fa-angle-left" style="width:20px; height: 30px;"></i>
+    </button>
+    <button class="btn btn-outline-dark fs-3">
+      <i class="fa-solid fa-angle-right" style="width:20px; height: 30px;"></i>
+    </button>
+  </div>
+  <div class="container mt-4">
+    <img src="img/Jumbo1.webp" class="img-fluid" alt="äge pilt">
+  </div> <!-- Tee kunagi carousel-ga -->
+
+  <div class="container mt-4">
+    <div class="display-1 fs-6 text-center">
+      Call to action text This is some text and button within a call-to-action text block.
+      <button class="ms-4 mr-1 btn btn-danger" style="border-radius: 4px; font-size: 14px; padding: 4px;">Call to
+        Action</button>
+    </div>
+  </div>
+  <p class="text-center" style="margin-top: 40px;margin-bottom: -5px; font-weight: 700; font-size: 50px">Get<span
+      style="color: red;"> Creative</span></p>
+  <div class="container">
+    <div class="display-1 fs-5 text-center">
+      Vivamus sagittis lacus vel augue laoreet rutrum faucivus dolor auctor. <a style="text-decoration:none;" href="#">
+        Duis mollis</a>.
+    </div>
+  </div>
+  <div class="container text-center" style="margin-top: 60px;">
+    <div class="row">
+      <div class="col">
+        <img src="img/Grid1.jpg" class="img-fluid" alt="äge pilt">
+      </div>
+      <div class="col">
+        <img src="img/Grid2.jpg" class="img-fluid" alt="äge pilt">
+      </div>
+    </div>
+    <div class="row mt-4 mb-4">
+      <div class="col">
+        <img src="img/Grid3.jpg" class="img-fluid" style="height: 300px;" alt="äge pilt">
+      </div>
+      <div class="col">
+        <img src="img/Grid4.jpg" class="img-fluid" style="height: 300px;" alt="äge pilt">
+      </div>
+      <div class="col">
+        <img src="img/Grid5.jpg" class="img-fluid" style="height: 300px;" alt="äge pilt">
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <img src="img/Grid6.jpg" class="img-fluid" style="height: 400px" alt="äge pilt">
+        <p class="fw-bold fs-1" style="margin-bottom: -10px;">Awesome Things</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam quaerat excepturi cupiditate fugit qui cumque!
+          Saepe omnis, nemo minima, repudiandae impedit sed expedita vel fugiat dolorem laudantium amet vitae nesciunt.
+        </p>
+      </div>
+      <div class="col">
+        <img src="img/Grid7.jpg" class="img-fluid" style="height: 400px" alt="äge pilt">
+        <p class="fw-bold fs-1 " style="margin-bottom: -10px;">More Awesomeness</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam quaerat excepturi cupiditate fugit qui cumque!
+          Saepe omnis, nemo minima, <a href="#">repudiandae</a> impedit sed expedita vel fugiat dolorem laudantium amet
+          vitae nesciunt.</p>
+      </div>
+    </div>
+  </div>
+  <div class="container mt-4">
+    <div class="row">
+      <div class="col">
+        <p class="fw-semibold fs-1" style="margin-bottom: -10px;">What we can do for you</p>
+        <div class="display-1 fs-4 mb-2">Here are some of our top features.</div>
+        <ul>
+          <li>
+            Add your own blocks
+          </li>
+          <li>
+            React Native app source included for Android and iOS
+          </li>
+          <li>
+            One year of free updates
+          </li>
+          <li>
+            Premium support
+          </li>
+          <li>
+            User friendly and intuitive
+          </li>
+          <li>
+            Media browser soon
+          </li>
+          <li>
+            Bootstrap 4 support
+          </li>
+        </ul>
+      </div>
+      <div class="col">
+        <img src="img/Grid8.jpg" class="img-fluid" style="height: 300px" alt="lahe pilt">
+      </div>
+    </div>
+  </div>
+  <p class="fw-semibold fs-1 text-center" style="margin-top: 80px">How We Work</p>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <img src="img/Grid9.jpg" class="img-fluid rounded-1" style="height: 300px" alt="äge pilt">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ex iure vero fugiat, sequi, doloribus
+          accusantium nobis tempora culpa quis vel perferendis repellendus odio eum exercitationem eos quo nulla.
+          Assumenda?</p>
+      </div>
+      <div class="col">
+        <img src="img/Grid10.jpg" class="img-fluid rounded-1" style="height: 300px" alt="äge pilt">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ex iure vero fugiat, sequi, doloribus
+          accusantium nobis tempora culpa quis vel perferendis repellendus odio eum exercitationem eos quo nulla. <a
+            href="#">Assumenda?</a> </p>
+      </div>
+      <div class="col">
+        <img src="img/Grid11.jpg" class="img-fluid rounded-1" style="height: 300px" alt="äge pilt">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ex iure vero fugiat, sequi, doloribus
+          accusantium nobis <a href="#">aempora culpa quis vel</a> perferendis repellendus odio eum exercitationem eos
+          quo nulla. Assumenda?</p>
+      </div>
+    </div>
+  </div>
+  <div class="container" style="margin-top: 80px;">
+    <div class="row row-cols-1 row-colds-md-4 g-4">
+      <div class="card-group">
+        <div class="row">
+          <div class="col">
+            <div class="card" style="box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); border-radius: 0; width:300px;">
+              <img src="./img/Grid12.jpg" class="card-img-top" alt="Grid 13" style="object-fit: cover; height: 250px;">
+              <div class="card-body center">
+                <h2 class="card-title">John Doe</h2>
+                <p class="card-text" style="color: gray;">UI/UX Designer</p>
+                <div class="d-flex justify-content-center mt-3 mb-3"> <!-- Added mt-3 class for top margin -->
+                  <i class="fab fa-twitter text-primary fs-4 me-3"></i> <!-- Twitter icon -->
+                  <i class="fab fa-facebook text-primary fs-4 me-3"></i> <!-- Facebook icon -->
+                  <i class="fab fa-google-plus text-primary fs-4 me-3"></i> <!-- Google+ icon -->
+                  <i class="fab fa-linkedin text-primary fs-4"></i> <!-- LinkedIn icon -->
+                </div>
+                <div class="d-flex justify-content-center">
+                  <a href="#" class="btn btn-outline-primary">Contact me!</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card" style="box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); border-radius: 0; width:300px;">
+              <img src="./img/Grid13.jpg" class="card-img-top" alt="Grid 13" style="object-fit: cover; height: 250px;">
+              <div class="card-body center">
+                <h2 class="card-title">Jane Doe</h2>
+                <p class="card-text" style="color: gray;">UI/UX Designer</p>
+                <div class="d-flex justify-content-center mt-3 mb-3"> <!-- Added mt-3 class for top margin -->
+                  <i class="fab fa-twitter text-primary fs-4 me-3"></i> <!-- Twitter icon -->
+                  <i class="fab fa-facebook text-primary fs-4 me-3"></i> <!-- Facebook icon -->
+                  <i class="fab fa-google-plus text-primary fs-4 me-3"></i> <!-- Google+ icon -->
+                  <i class="fab fa-linkedin text-primary fs-4"></i> <!-- LinkedIn icon -->
+                </div>
+                <div class="d-flex justify-content-center">
+                  <a href="#" class="btn btn-outline-primary">Contact me!</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card" style="box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); border-radius: 0; width:300px;">
+              <img src="./img/Grid14.jpg" class="card-img-top" alt="Grid 14" style="object-fit: cover; height: 250px;">
+              <div class="card-body center">
+                <h2 class="card-title">John Doe</h2>
+                <p class="card-text" style="color: gray;">UI/UX Designer</p>
+                <div class="d-flex justify-content-center mt-3 mb-3"> <!-- Added mt-3 class for top margin -->
+                  <i class="fab fa-twitter text-primary fs-4 me-3"></i> <!-- Twitter icon -->
+                  <i class="fab fa-facebook text-primary fs-4 me-3"></i> <!-- Facebook icon -->
+                  <i class="fab fa-google-plus text-primary fs-4 me-3"></i> <!-- Google+ icon -->
+                  <i class="fab fa-linkedin text-primary fs-4"></i> <!-- LinkedIn icon -->
+                </div>
+                <div class="d-flex justify-content-center">
+                  <a href="#" class="btn btn-outline-primary">Contact me!</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card" style="box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); border-radius: 0; width:300px;">
+              <img src="./img/Grid15.jpg" class="card-img-top" alt="Grid 15" style="object-fit: cover; height: 250px;">
+              <div class="card-body center">
+                <h2 class="card-title">Jane Doe</h2>
+                <p class="card-text" style="color: gray;">UI/UX Designer</p>
+                <div class="d-flex justify-content-center mt-3 mb-3"> <!-- Added mt-3 class for top margin -->
+                  <i class="fab fa-twitter text-primary fs-4 me-3"></i> <!-- Twitter icon -->
+                  <i class="fab fa-facebook text-primary fs-4 me-3"></i> <!-- Facebook icon -->
+                  <i class="fab fa-google-plus text-primary fs-4 me-3"></i> <!-- Google+ icon -->
+                  <i class="fab fa-linkedin text-primary fs-4"></i> <!-- LinkedIn icon -->
+                </div>
+                <div class="d-flex justify-content-center">
+                  <a href="#" class="btn btn-outline-primary">Contact me!</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container" style="margin-top: 120px;">
+        <h2 style="font-weight: bold;">From our blog</h2>
+        <div class="row row-cols-1 row-colds-md-4 g-4">
+          <div class="card-group">
+            <div class="row">
+              <div class="col">
+                <div class="card" style="box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); border-radius: 0; width:300px;">
+                  <img src="./img/Grid12.jpg" class="card-img-top" alt="Grid 13"
+                    style="object-fit: cover; height: 250px;">
+                  <div class="card-body">
+                    <h2 class="card-title" style="color: blue;">Blog post title</h2>
+                    <span style="color: blue;">July 30rd 2018</span>
+                    <span>|</span>
+                    <span style="color: blue;">5 comments</span>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit eum officia voluptate
+                      praesentium dolores? Corporis unde error dignissimos ducimus quis inventore nihil necessitatibus,
+                      aspernatur numquam molestiae maiores distinctio ipsam nam.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                <div class="card" style="box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); border-radius: 0; width:300px;">
+                  <img src="./img/Grid12.jpg" class="card-img-top" alt="Grid 13"
+                    style="object-fit: cover; height: 250px;">
+                  <div class="card-body">
+                    <h2 class="card-title" style="color: blue;">Blog post title</h2>
+                    <span style="color: blue;">July 30rd 2018</span>
+                    <span>|</span>
+                    <span style="color: blue;">5 comments</span>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit eum officia voluptate
+                      praesentium dolores? Corporis unde error dignissimos ducimus quis inventore nihil necessitatibus,
+                      aspernatur numquam molestiae maiores distinctio ipsam nam.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                <div class="card" style="box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); border-radius: 0; width:300px;">
+                  <img src="./img/Grid12.jpg" class="card-img-top" alt="Grid 13"
+                    style="object-fit: cover; height: 250px;">
+                  <div class="card-body">
+                    <h2 class="card-title" style="color: blue;">Blog post title</h2>
+                    <span style="color: blue;">July 30rd 2018</span>
+                    <span>|</span>
+                    <span style="color: blue;">5 comments</span>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit eum officia voluptate
+                      praesentium dolores? Corporis unde error dignissimos ducimus quis inventore nihil necessitatibus,
+                      aspernatur numquam molestiae maiores distinctio ipsam nam.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                <div class="card" style="box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); border-radius: 0; width:300px;">
+                  <img src="./img/Grid12.jpg" class="card-img-top" alt="Grid 13"
+                    style="object-fit: cover; height: 250px;">
+                  <div class="card-body">
+                    <h2 class="card-title" style="color: blue;">Blog post title</h2>
+                    <span style="color: blue;">July 30rd 2018</span>
+                    <span>|</span>
+                    <span style="color: blue;">5 comments</span>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit eum officia voluptate
+                      praesentium dolores? Corporis unde error dignissimos ducimus quis inventore nihil necessitatibus,
+                      aspernatur numquam molestiae maiores distinctio ipsam nam.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="container" style="margin-top:80px; margin-bottom:20px;">
+            <div class="d-flex justify-content-center">
+              <a href="#" class="btn btn-outline-dark btn-lg">View all blog posts</a>
+            </div>
+          </div>
+</body>
+
+</html>
